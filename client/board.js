30090a4c76cd668cabcf72332316e918d6039df6
Ruben Homs <rubenhoms@gmail.com>
2012-11-03 19:41:42 +0100

    Refactor to event driven instead of setInterval

--- client/board.js
+++ client/board.js
@@ -1,94 +1,110 @@
 Template.gameDice.events({
-    'click input.dice': function () {
-        if(!Session.get('gameid')){
+    'click input.dice':function () {
+        if (!Session.get('gameid')) {
             console.log("GameID not set");
             return;
         }
-        if(Dice.findOne({'access_code' : Session.get('gameid')})) {
-            Dice.update({'access_code' : Session.get('gameid')}, {$set: {'throw': Math.floor(Math.random() * 3)}});
+        if (Dice.findOne({'access_code':Session.get('gameid')})) {
+            Dice.update({'access_code':Session.get('gameid')}, {$set:{'throw':Math.floor(Math.random() * 3)}});
         } else {
-            Dice.insert({'access_code' : Session.get('gameid'), 'throw': Math.floor(Math.random() * 3 )});
+            Dice.insert({'access_code':Session.get('gameid'), 'throw':Math.floor(Math.random() * 3)});
         }
     }
 });
 
 Template.gameDice.diceThrow = function () {
-    return Dice.findOne({'access_code' : Session.get('gamecode')});
+    return Dice.findOne({'access_code':Session.get('gamecode')});
 }
 
 var player = function () {
-  return Players.findOne(Session.get('player_id'));
+    return Players.findOne(Session.get('player_id'));
 };
 
 var game = function () {
-  var me = player();
-  return me && me.gamecode && Games.findOne(me.gamecode);
+    var me = player();
+    return me && me.gamecode && Games.findOne(me.gamecode);
 };
 
 Template.lobby.events({
-  'click input.startgame': function () {
-  Meteor.call('start_new_game', function(error,gamecode) {
-    Template.showcode.gamecode = gamecode;
-      var fragment = Meteor.render(Template.showcode);
-      $("body").html(fragment);
-    });
-  },
-  'click input.joingame': function () {
-     $("body").html(Meteor.render(Template.join));
-  }
+    'click input.startgame':function () {
+        Meteor.call('start_new_game', function (error, gamecode) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            Template.showcode.gamecode = gamecode;
+            var fragment = Meteor.render(Template.showcode);
+            $("body").html(fragment);
+        });
+    },
+    'click input.joingame':function () {
+        $("body").html(Meteor.render(Template.join));
+    }
 });
 
 Template.join.events({
-    'click input.joingame': function() {
-	  gamecode = $("#gamecode").val();
-      Meteor.call('joined_game', gamecode ,function(error,result) {
-         $("body").html(Meteor.render(Template.joined));
-      });
+    'click input.joingame':function () {
+        gamecode = $("#gamecode").val();
+        Meteor.call('joined_game', gamecode, function (error, result) {
+            if(error) {
+                console.log(error);
+                return;
+            }
+            $("body").html(Meteor.render(Template.joined));
+        });
     }
 });
 
-Meteor.startup(function () {
-  // Allocate a new player id.
-  //
-  // XXX this does not handle hot reload. In the reload case,
-  // Session.get('player_id') will return a real id. We should check for
-  // a pre-existing player, and if it exists, make sure the server still
-  // knows about us.
-  var player_id = Players.insert({name: '', idle: false});
-  Session.set('player_id', player_id);
-
-  // subscribe to all the players, the game i'm in, and all
-  // the words in that game.
-  Meteor.autosubscribe(function () {
-    Meteor.subscribe('players');
-
-    if (Session.get('player_id')) {
-      var me = player();
-      if (me && me.gamecode) {
-        Meteor.subscribe('games', me.gamecode);
-        Session.set('gameid', me.gamecode);
-      }
+Template.joined.ready = function () {
+    var game = Games.findOne({'gamecode':Session.get('gameid')});
+    if (game) {
+        if (game.players.length >= 2) {
+            $("body").html(Meteor.render(Template.gameDice));
+        }
     }
-  });
+}
 
-  // send keepalives so the server can tell when we go away.
-  //
-  // XXX this is not a great idiom. meteor server does not yet have a
-  // way to expose connection status to user code. Once it does, this
-  // code can go away.
-  Meteor.setInterval(function() {
-    if (Meteor.status().connected)
-      Meteor.call('keepalive', Session.get('player_id'));
-  }, 20*1000);
-  
-  // this is not a great idiom. REFACTOR PLZ
-  Meteor.setInterval(function() {
-  		player = Players.findOne(Session.get('player_id'));
-  		if(typeof player.gamecode!='undefined' && player.gamecode.length) {
-  			game = Games.findOne({gamecode: player.gamecode});
-			console.log('There are '+game.players.length+' in the game!');
-  		} else {
-  			console.log('Player not yet in game.');
-  		}
-  }, 1000);
+Template.showcode.ready = function () {
+    var game = Games.findOne({'gamecode' : Session.get('gameid')});
+    if (game) {
+        if (game.players.length >= 2) {
+            $("body").html(Meteor.render(Template.gameDice));
+        }
+    }
+}
+
+Meteor.startup(function () {
+    // Allocate a new player id.
+    //
+    // XXX this does not handle hot reload. In the reload case,
+    // Session.get('player_id') will return a real id. We should check for
+    // a pre-existing player, and if it exists, make sure the server still
+    // knows about us.
+    var player_id = Players.insert({name:'Henk', idle:false});
+    Session.set('player_id', player_id);
+
+    // subscribe to all the players, the game i'm in, and all
+    // the words in that game.
+    Meteor.autosubscribe(function () {
+        Meteor.subscribe('players');
+
+        if (Session.get('player_id')) {
+            var me = player();
+            if (me && me.gamecode) {
+                Meteor.subscribe('games', me.gamecode);
+                Session.set('gameid', me.gamecode);
+            }
+        }
+    });
+
+    // send keepalives so the server can tell when we go away.
+    //
+    // XXX this is not a great idiom. meteor server does not yet have a
+    // way to expose connection status to user code. Once it does, this
+    // code can go away.
+    Meteor.setInterval(function () {
+        if (Meteor.status().connected)
+            Meteor.call('keepalive', Session.get('player_id'));
+    }, 20 * 1000);
+
 });
--- server/game.js
+++ server/game.js
@@ -1,106 +1,111 @@
 ////////// Server only logic //////////
 function player() {
-  return Players.findOne(Session.get('player_id'));
+    return Players.findOne(Session.get('player_id'));
 }
 
 var game = function () {
-  var me = player();
-  return me && me.gamecode && Games.findOne(me.gamecode);
+    var me = player();
+    return me && me.gamecode && Games.findOne(me.gamecode);
 };
 
-Meteor.methods({
-  start_new_game: function (evt) {
-	var clock = 42;
 function createGamecode() {
     var gamecode = '';
-	for(i=0;i<3;i++) {
-		if(i==0) {
-			 // don't allow 0 as first digit
-			random = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
-		} else {
-			random = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
-		}
-		gamecode += ''+random;
-	}
-	found = Games.findOne({'gamecode':gamecode});
-	if(found) {
-		return createGameCode();
-	}
-	return gamecode;
-}
-	gamecode = createGamecode();
-
-    // create a new game with the current player in it
-    Games.insert({player: player(), clock: clock, gamecode: gamecode});
-    
-    // move everyone who is ready in the lobby to the game
-    Players.update({gamecode: null, idle: false, player_id: Session.get('player_id')},
-                   {$set: {gamecode: gamecode}},
-                   {multi: true});
-                   
-    // Save a record of who is in the game, so when they leave we can
-    // still show them.
-    var p = Players.find({gamecode: gamecode},
-                         {fields: {_id: true, name: true}}).fetch();
-    Games.update({gamecode: gamecode}, {$set: {players: p}});
-	
-    // wind down the game clock
-    var interval = Meteor.setInterval(function () {
-      clock -= 1;
-      Games.update({gamecode: gamecode}, {$set: {clock: clock}});
-
-      // end of game
-      if (clock === 0) {
-        // stop the clock
-        Meteor.clearInterval(interval);
-        // declare zero or more winners
-        var scores = {};
-
-        /*
-        var high_score = _.max(scores);
-        var winners = [];
-        _.each(scores, function (score, player_id) {
-          if (score === high_score)
-            winners.push(player_id);
-        });
-        Games.update({gamecode:gamecode}, {$set: {winners: winners}});
-        */
-      }
-    }, 1000);
-    
-    Session.set('gamecode',gamecode);
-
+    for (i = 0; i < 3; i++) {
+        if (i == 0) {
+            // don't allow 0 as first digit
+            random = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+        } else {
+            random = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+        }
+        gamecode += '' + random;
+    }
+    found = Games.findOne({'gamecode':gamecode});
+    if (found) {
+        return createGameCode();
+    }
     return gamecode;
-  }, keepalive: function (player_id) {
-    Players.update({_id: player_id},
-                  {$set: {last_keepalive: (new Date()).getTime(),
-                          idle: false}});
-  },
+}
 
-  joined_game: function (gamecode) {
-    // move everyone who is ready in the lobby to the game
-    Players.update({gamecode: null, idle: false, player_id: Session.get('player_id')},
-                   {$set: {gamecode: gamecode}},
-                   {multi: true});
-    // Save a record of who is in the game, so when they leave we can
-    // still show them.
-    var p = Players.find({gamecode: gamecode},
-                         {fields: {_id: true, name: true}}).fetch();
-    Games.update({gamecode:gamecode}, {$set: {players: p}});
-    
-    return 'joined';
-  }
+Meteor.methods({
+    start_new_game:function (evt) {
+        var clock = 42;
+        gamecode = createGamecode();
+
+        // create a new game with the current player in it
+        Games.insert({player : player(), clock : clock, 'gamecode' : gamecode});
+
+        // move everyone who is ready in the lobby to the game
+        Players.update({'gamecode':null, idle:false, player_id:Session.get('player_id')},
+            {$set:{'gamecode' : gamecode}},
+            {multi:true});
+
+        // Save a record of who is in the game, so when they leave we can
+        // still show them.
+        var p = Players.find({gamecode:gamecode},
+            {fields:{_id:true, name:true}}).fetch();
+        Games.update({gamecode:gamecode}, {$set:{players:p}});
+
+        // wind down the game clock
+        var interval = Meteor.setInterval(function () {
+            clock -= 1;
+            Games.update({'gamecode' : gamecode}, { $set : { clock : clock}});
+
+            // end of game
+            if (clock === 0) {
+                // stop the clock
+                Meteor.clearInterval(interval);
+                // declare zero or more winners
+                var scores = {};
+
+                /*
+                 var high_score = _.max(scores);
+                 var winners = [];
+                 _.each(scores, function (score, player_id) {
+                 if (score === high_score)
+                 winners.push(player_id);
+                 });
+                 Games.update({gamecode:gamecode}, {$set: {winners: winners}});
+                 */
+            }
+        }, 1000);
+
+        Session.set('gamecode', gamecode);
+
+        return gamecode;
+    },
+
+    keepalive: function ( player_id ) {
+        Players.update({_id:player_id},
+            {$set:{last_keepalive:(new Date()).getTime(),
+                idle:false}});
+    },
+
+    joined_game:function (gamecode) {
+        // move everyone who is ready in the lobby to the game
+        Players.update({gamecode:null, idle:false, player_id:Session.get('player_id')},
+            {$set:{'gamecode' : gamecode}},
+            {multi:true});
+        // Save a record of who is in the game, so when they leave we can
+        // still show them.
+        var p = Players.find({'gamecode' : gamecode},
+            {fields : {_id:true, name:true}}).fetch();
+        Games.update({gamecode:gamecode}, {$set:{players:p}});
+
+        if (Games.findOne({'gamecode':gamecode}).players.length >= 2) {
+            return true;
+        }
+    }
 });
 
 Meteor.setInterval(function () {
-  var now = (new Date()).getTime();
-  var idle_threshold = now - 70*1000; // 70 sec
-  var remove_threshold = now - 60*60*1000; // 1hr
+    var now = (new Date()).getTime();
+    var idle_threshold = now - 70 * 1000; // 70 sec
+    var remove_threshold = now - 60 * 60 * 1000; // 1hr
 
-  Players.update({$lt: {last_keepalive: idle_threshold}},
-                 {$set: {idle: true}});
+    Players.update({$lt:{last_keepalive:idle_threshold}},
+        {$set:{idle:true}});
 
-  // XXX need to deal with people coming back!
-  // Players.remove({$lt: {last_keepalive: remove_threshold}});
+    // XXX need to deal with people coming back!
+    // Players.remove({$lt: {last_keepalive: remove_threshold}});
 
-}, 30*1000);
+}, 30 * 1000);
