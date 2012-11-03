be039bf800a4352472358d06ec1d398185c8a27e
Peter Jaap Blaakmeer <info@blaakmeer.com>
2012-11-03 15:31:41 +0100

    Create new game with returned gamecode works\!

--- client/board.js
+++ client/board.js
@@ -1,8 +1,50 @@
+var player = function () {
+  return Players.findOne(Session.get('player_id'));
+};
+
 Template.lobby.events({
-  'click button.startgame': function () {
-    Meteor.call('start_new_game');
+  'click input.startgame': function () {
+	Meteor.call('start_new_game', function(error,gamecode) {
+		Template.wait.gamecode = gamecode;	
+    	var fragment = Meteor.render(Template.wait);
+	    $("body").html(fragment);
+    });
   },
-  'click button.joingame': function () {
+  'click input.joingame': function () {
     Meteor.call('join_game');
   }
+});
+
+Meteor.startup(function () {
+  // Allocate a new player id.
+  //
+  // XXX this does not handle hot reload. In the reload case,
+  // Session.get('player_id') will return a real id. We should check for
+  // a pre-existing player, and if it exists, make sure the server still
+  // knows about us.
+  var player_id = Players.insert({name: '', idle: false});
+  Session.set('player_id', player_id);
+
+  // subscribe to all the players, the game i'm in, and all
+  // the words in that game.
+  Meteor.autosubscribe(function () {
+    Meteor.subscribe('players');
+
+    if (Session.get('player_id')) {
+      var me = player();
+      if (me && me.game_id) {
+        Meteor.subscribe('games', me.game_id);
+      }
+    }
+  });
+
+  // send keepalives so the server can tell when we go away.
+  //
+  // XXX this is not a great idiom. meteor server does not yet have a
+  // way to expose connection status to user code. Once it does, this
+  // code can go away.
+  Meteor.setInterval(function() {
+    if (Meteor.status().connected)
+      Meteor.call('keepalive', Session.get('player_id'));
+  }, 20*1000);
 });
