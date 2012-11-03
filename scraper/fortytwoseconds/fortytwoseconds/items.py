92dcfe59423dd0fca57b4a274098e1d46125d8aa
Pascal Touset <p.b.touset@gmail.com>
2012-11-03 14:08:53 +0100

    scraper renames/fixes

--- scraper/fortytwoseconds/fortytwoseconds/items.py
+++ scraper/fortytwoseconds/fortytwoseconds/items.py
@@ -1,12 +1,7 @@
 from scrapy.item import Item, Field
 
-class FortytwosecondsItem(Item):
+class FortyTwoSecondsItem(Item):
     answer = Field()
     link = Field()
     language = Field()
     category = Field()
-
-class DmozItem(Item):
-    title = Field()
-    link = Field()
-    desc = Field()
