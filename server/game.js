a6ef12bb8ef97314f661a8e2dceddebf06abd463
Peter Jaap Blaakmeer <info@blaakmeer.com>
2012-11-04 17:48:56 +0100

    Fix gamecode arg in loadANswers

--- server/game.js
+++ server/game.js
@@ -159,7 +159,7 @@ Meteor.methods({
                         Games.update({gamecode:gamecode}, {$set:{'team':team}});
                     }
                 }
-                loadAnswers();
+                loadAnswers(gamecode);
                 if(game.round>=game.rounds) {
                 	// game ENDS!
                     // declare zero or more winners
