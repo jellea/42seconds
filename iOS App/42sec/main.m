59a89a55cf2c5eb22febf753835738ef11cad650
Joshua Peper <joshpeper@me.com>
2012-12-01 15:53:20 +0100

    iOS app

---
+++ iOS App/42sec/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  42sec
+//
+//  Created by Joshua Peper on 01-12-12.
+//  Copyright (c) 2012 Joshua Peper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTSAppDelegate class]));
+    }
+}
